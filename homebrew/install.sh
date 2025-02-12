5014c6cfaf863019dd02b393d0f288cf9c4a3b13
Ghasem Shirazi <gshirazi@infoblox.com>
2025-02-12 01:00:03 -0800

    Update install.sh

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -11,10 +11,10 @@ then
   echo "Didn't find it! Installing Homebrew for you."
   if test "$(uname)" = "Darwin"
   then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/refs/head/install/master/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/HEAD/install/master/install)"
   elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
   then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/refs/head/install/master/install)"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/HEAD/install/master/install)"
   fi
 fi
 
