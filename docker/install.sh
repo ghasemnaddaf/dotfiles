e83ee7b25079599277669f3fa23576eb4b0a1efa
Ghasem Shirazi <gshirazi@infoblox.com>
2025-02-19 01:18:41 -0800

    fix some errors

--- docker/install.sh
+++ docker/install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+DOCKER_DESKTOP_BINARY=/Applications/Docker.app/Contents/MacOS/Docker\ Desktop.app/Contents/MacOS/Docker\ Desktop
 if test "$(uname)" = "Darwin"; then
-    if [[ -f /Applications/Docker.app/Contents/MacOS/Docker ]]; then
+    if [[ -f ${ DOCKER_DESKTOP_BINARY} ]]; then
         echo "docker is already installed, just run it!"
     else
-        wget https://desktop.docker.com/mac/main/arm64/Docker.dmg
-        sudo hdiutil attach Docker.dmg
+        if [[ ! -f /tmp/Docker.dmg ]]; then wget https://desktop.docker.com/mac/main/arm64/Docker.dmg -O /tmp/Docker.dmg; fi
+        sudo hdiutil attach /tmp/Docker.dmg
         sudo /Volumes/Docker/Docker.app/Contents/MacOS/install --accept-license
         sudo hdiutil detach /Volumes/Docker
+	rm -rf /tmp/Docker.dmg
     fi
-    /Applications/Docker.app/Contents/MacOS/Docker &
+    ${DOCKER_DESKTOP_BINARY} &
 else
     # TODO: install docker if not installed
     sudo adduser $USER docker
