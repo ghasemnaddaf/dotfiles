9c4efc0e1a555b66b764fdb2f7c99f109f9fa4b6
Ghasem Shirazi <gshirazi@infoblox.com>
2025-02-20 14:28:25 -0800

    more teleport aliases

--- tsh/tsh.zsh
+++ tsh/tsh.zsh
@@ -9,16 +9,66 @@ func tplogin() {
 	   tpls
    fi
 }
+
+alias tp1='tplogin env-1'
 alias tp2='tplogin env-2a'
 alias tp4='tplogin env-4'
 alias tp5='tplogin env-5'
 alias tpp='tplogin prd-1'
 alias tp0='tpp'
 alias tps='tplogin stg-1'
+alias tpbox4='tplogin box-4'
 alias tpdd='tplogin ddi-dev-use1'
 alias tpd5='tpdd'
 alias tpdq='tplogin ddi-qa-use1'
+alias tpqaps1='tplogin ddi-qa-aps1'
 alias tpd2='tpdq'
 alias tpd2a='tpd2'
 alias tpd4='tplogin ddi-qa-ase1'
 alias tpb4='tplogin box-4'
+alias tpdaps='tplogin ddi-dev-aps1'
+alias tpdase='tpologin ddi-dev-ase1'
+alias tpdsgp='tplogin ddi-dev-sgp1'
+alias tpsuse='tplogin ddi-stg-use1'
+alias tp0mmb='tplogin ddi-prd-mmb1'
+alias tp0frk='tplogin ddi-prd-frk1'
+alias tp0nva='tplogin ddi-prd-nva1'
+alias tp0spa='tplogin ddi-prd-spa1'
+
+alias tpu='tsh kube --proxy=${TELEPORT_EU_PROXY}'
+alias tpuls='tpu ls'
+alias tpuli='tpu login --set-context-name'
+func tpulogin() {
+   local cluster=$1
+   local user=$1
+   if ! tpuli $cluster $user; then
+           echo "failed to teleport to $cluster/$user via ${TELEPORT_EU_PROXY} list of available clusters:"
+           tpuls
+   fi
+}
+
+alias tpustg='tpulogin eu-stg-1'
+alias tpus='tpustg'
+alias tpu0='tpulogin eu-com-1'
+alias tpu0frk='tpulogin ddi-eu-prd-frk1'
+
+alias tpg='tsh kube --proxy=${TELEPORT_GCP_PROXY}'
+alias tpgls='tpg ls'
+alias tpgli='tpg login'
+func tpglogin() {
+    local cluster=$1
+       if ! tpgli $cluster; then
+           echo "failed to teleport to $cluster via ${TELEPORT_GCP_PROXY} list of available clusters:"
+           tpgls
+   fi
+}
+alias tpgduse1='tpglogin gcp-ddi-dev-use1'
+alias tpgd='tpguse1'
+alias tpgdase='tpglogin gcp-ddi-dev-ase1'
+alias tpgquse='tpglogin gcp-ddi-qa-use1'
+alias tpgq='tpguse1'
+alias tpgsase='tpglogin gcp-ddi-stg-ase1'
+alias tpg0ogn='tplogin gcp-ddi-prd-ogn1'
+alias tpg0ldn='tplogin gcp-ddi-eu-prd-ldn1'
+alias tpg0eu='tpg0ldn'
+
