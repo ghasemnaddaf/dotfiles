15eb32035b376e7a467274493f736db813b530d0
Ghasem Shirazi <gshirazi@infoblox.com>
2025-03-07 10:39:13 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ function git_clone() {
   git clone git@github.com:$1.git $2
 }
 
-PKG_LIST_COMMON="awscli flake8 git golang packer teleport tree vim wget zsh"
+PKG_LIST_COMMON="awscli flake8 git golang packer teleport tree vim nmap wget zsh"
 PKG_LIST_MAC="jq go gnupg vim"
 PKG_LIST_CASK_MAC="google-chrome iterm2 slack flux sublime-text"
 PKG_LIST_LINUX="build-essential cmake curl dmidecode docker.io python3 python3-pip virtualbox"
